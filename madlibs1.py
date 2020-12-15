c353eb360b292ddf58b4a47a47f97b1b1c1b9637
Seth <76057266+Seth-Source@users.noreply.github.com>
2020-12-15 15:29:16 -0300

    Add files via upload

---
+++ madlibs1.py
@@ -0,0 +1,32 @@
+    #####################
+     ##MADLIBS by Seth##
+    #####################
+
+#History
+print("It was a \"adjective\", cold November day.")
+print("I woke up to the \"adjective2\" smell of \"type of bird\" roasting in the \"room in a house\" downstairs.")
+print("I \"verb1 (past tense)\" down the stairs to see if i could help \"verb2\" the dinner.")
+print("My mom said, \"See if \"relative's name\" needs a fresh \"noun\".\" So I carried a tray of glasses full of \"a liquid\" into the \"verb ending in -ing\" room.")
+print("When I got there, I couldn't believe my \"part of the body (plural)\"!")
+print("There were \"plural noun \" \"verb ending in -ing\" on the \"noun\"!")
+#Variables
+adj1 = input("Enter the first adjective: ")
+adj2 = input("Enter the second adjective: ")
+type_of_bird = input("Enter the tipe of bird: ")
+room_in_a_house = input("Enter the room in a house: " )
+verb1 = input("Enter the first verb: ")
+verb2 = input("Enter the second verb: ")
+relative_name = input("Enter the relative's name: ")
+noun1 = input("Enter the first noun: ")
+a_liquid = input("Enter a liquid: ")
+verb_ing = input("Enter the first verb -ing: ")
+part_of_the_body_plural = input("Enter the part of the body (plural): ")
+plural_noun2 = input("Enter the second plural noun: ")
+verb_ing2 = input("Enter the second verb -ing: ")
+noun2 = input("Enter the second noun: ")
+#Full History
+print("It was a " + adj1 + " cold November day.")
+print("I woke up to the " + adj2 + " smell of " + type_of_bird + " roasting in the " + room_in_a_house + " Downstairs.")
+print("I " + verb1 + " down the stairs to see if I could help " + verb2 + " the dinner.")
+print("My mom said, \"See if\" " + relative_name + " needs a fresh " + noun1 + ". So I carried a tray of glasses full of " + a_liquid + " into the " + verb_ing + " room. When I got there, I couldn't believe my " + part_of_the_body_plural + "!")
+print("There were " + plural_noun2 + " " + verb_ing2+ " on the " + noun2 + "!")
